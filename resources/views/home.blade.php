62cda01087a3d4f652f846622f9e721d6bed29f8
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-03-11 18:39:49 +0100

    added_database

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+
+</head>
+
+<body>
+    <header>
+        <h1>placeholder</h1>
+    </header>
+    <main>
+        <p>placeholder</p>
+    </main>
+
+</body>
+
+</html>
