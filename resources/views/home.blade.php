adb0bb37ca5b563c87539b269cc4740e3639bc41
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-03-12 20:55:20 +0100

    images_added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,10 +18,17 @@
 
     <main>
         <div class="container">
-            <h2 class="text-center py-3">Lista fumetti in archivio</h2>
-            <ul class="d-flex flex-wrap">
+            <h2 class="text-center py-3 m-0">Lista fumetti in archivio</h2>
+            <ul class="row">
                 @foreach ($comics as $comic)
-                    <li class="list-unstyled col-3 py-2">{{ $comic['series'] }}</li>
+                    <li class="list-unstyled col-3 p-2 text-center">
+                        <div class="card">
+                            <img src="{{ $comic['thumb'] }}" alt="{{ $comic['series'] }} cover">
+                            <div class="card-body">
+                                <p>{{ $comic['series'] }}</p>
+                            </div>
+                        </div>
+                    </li>
                 @endforeach
             </ul>
         </div>
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,10 +1,10 @@
 <header>
-    <div class="container">
-        <img :src="{{ asset('img/dc-logo.png') }}" :alt="DC Logo">
-        <ul class="d-flex justify-content-evenly">
+    <div class="container d-flex align-items-center justify-content-evenly">
+        <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC Logo" class="py-3">
+        <ul class="d-flex justify-content-evenly col-10">
             @foreach (config('headerUL') as $link)
                 <li class="list-unstyled">
-                    <a href="{{ url($link['route_name']) }}" class="{{ Route::is($link['route_name']) ? active : '' }}">
+                    <a href="{{ url($link['route_name']) }}" {{-- class="{{ Route::is($link['route_name']) ? active : '' }}" --}}>
                         {{ $link['text'] }}
                     </a>
                 </li>
